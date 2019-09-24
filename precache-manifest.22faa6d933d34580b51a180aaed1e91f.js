81f70d984f759f24deaaa1c04f5c2615be20df95
Neyasbltb <neyasbltb_88@mail.ru>
2019-09-25 02:17:31 +0300

    Updates

---
+++ precache-manifest.22faa6d933d34580b51a180aaed1e91f.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f710bc7c77096a21f2138fb40f66d1d",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "5a17212837dae8ffe95c",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d59ca812e9be4e42c7b6.bundle.js"
+  },
+  {
+    "url": "runtime~main.d59ca812e9be4e42c7b6.bundle.js"
+  },
+  {
+    "url": "vendors~main.d59ca812e9be4e42c7b6.bundle.js"
+  }
+]);
